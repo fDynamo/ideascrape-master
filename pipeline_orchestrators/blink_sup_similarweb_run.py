506737f5a06a72d7d8ed458b49c7c53ea355aab7
fDynamo <fontaine.dynamo@gmail.com>
2024-02-25 05:12:24 -0800

    blink sup similarweb runner

---
+++ pipeline_orchestrators/blink_sup_similarweb_run.py
@@ -0,0 +1,43 @@
+import subprocess
+from pipeline_orchestrators.blink_sup_similarweb_create_script import (
+    main as blink_sup_similarweb_create_script,
+)
+import argparse
+from custom_helpers_py.pipeline_components_helpers import get_components_from_script
+
+
+def main():
+    # Get arguments
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-s", "--start-index", type=int)
+    args, _ = parser.parse_known_args()
+
+    start_index = args.start_index
+
+    # Generate blink_sup_similarweb script
+    blink_sup_similarweb_script_filepath = blink_sup_similarweb_create_script()
+    if not blink_sup_similarweb_script_filepath:
+        return
+
+    # Get components from script
+    com_list = get_components_from_script(blink_sup_similarweb_script_filepath)
+
+    # Run all scripts
+    for i, com in enumerate(com_list):
+        if start_index and i < start_index:
+            continue
+
+        print("[ORCHESTRATOR] START script", i, com)
+
+        process = subprocess.run(com, shell=True)
+        returncode = process.returncode
+        if returncode > 0:
+            print("[ORCHESTRATOR] ERROR", i, com)
+            break
+
+        print("[ORCHESTRATOR] END script", i, com)
+
+
+if __name__ == "__main__":
+    main()
