fec4735d87aca720088135c109f4fe989f74f0b1
fDynamo <fontaine.dynamo@gmail.com>
2024-02-25 22:30:44 -0800

    pi update search main ids

---
+++ prod_interface/update-search-main-ids.mjs
@@ -0,0 +1,81 @@
+import createSupabaseClient from "../custom_helpers_js/create-supabase-client.js";
+import { readCsvFile } from "../custom_helpers_js/read-csv.js";
+import * as dotenv from "dotenv";
+import yargs from "yargs/yargs";
+import { hideBin } from "yargs/helpers";
+import { writeFileSync } from "fs";
+import { join } from "path";
+
+/**
+ * This currently just works for similarweb ids for blink-remap-search-main-ids
+ *
+ */
+async function main() {
+  dotenv.config();
+
+  const argv = yargs(hideBin(process.argv)).argv;
+  let { inputFilePath, prod, recordsFolder } = argv;
+  if (!inputFilePath) {
+    console.error("Invalid inputs");
+    process.exit(1);
+  }
+
+  const errorList = [];
+  let countSuccess = 0;
+  let countError = 0;
+  try {
+    const inRecords = await readCsvFile(inputFilePath);
+    let supabase = createSupabaseClient(prod);
+
+    for (let i = 0; i < inRecords.length; i++) {
+      const record = inRecords[i];
+      console.log("Updating", i, record);
+
+      let { id, similarweb_id } = record;
+      if (!similarweb_id) {
+        similarweb_id = null;
+      } else {
+        similarweb_id = Math.floor(similarweb_id);
+      }
+
+      const { error } = await supabase
+        .from("search_main")
+        .update({ similarweb_id })
+        .eq("id", id);
+
+      if (error) {
+        countError += 1;
+        errorList.push({
+          record,
+          error,
+        });
+      } else {
+        countSuccess += 1;
+      }
+
+      console.log("Finish", i, record);
+    }
+
+    if (recordsFolder) {
+      const toWriteObj = {
+        countSuccess,
+        countError,
+        errorList,
+      };
+
+      const toWrite = JSON.stringify(toWriteObj);
+      const fileSuffix = prod ? "prod.json" : "local.json";
+      const recordsFilePath = join(
+        recordsFolder,
+        "update_search_main_ids_" + fileSuffix
+      );
+
+      writeFileSync(recordsFilePath, toWrite);
+    }
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+main();
